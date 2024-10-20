88877dab7319dd20db9fc88ba7e01ec40f5aa167
mauguetta <mauriciolopezasencio@gmail.com>
2024-10-19 21:53:20 -0600

    configuracion de cors para deplegar aplicacion

--- src/main/java/com/spring/security/jwt/config/CorsConfig.java
+++ src/main/java/com/spring/security/jwt/config/CorsConfig.java
@@ -16,12 +16,26 @@ public class CorsConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())  // Desactiva CSRF con la nueva sintaxis
-                .cors(cors -> cors.disable())  // Habilitar o deshabilitar CORS según sea necesario
+                .csrf(csrf -> csrf.disable())  // Desactiva CSRF
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))  // Habilita CORS con tu configuración personalizada
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()  // Permite todas las solicitudes sin autenticación
                 );
 
         return http.build();
+    }
+
+    // Configuración de CORS personalizada
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://bitacora-front.onrender.com/")); // Permite solicitudes del frontend (Angular)
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos permitidos
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // Cabeceras permitidas
+        configuration.setAllowCredentials(true);  // Permite el envío de cookies o credenciales
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);  // Aplica CORS para todas las rutas
+        return source;
     }
 }
